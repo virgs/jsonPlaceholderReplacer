c0bfc182087db4a3d5abb348d7364822fdc02e61
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-04-18 14:01:31 -0300

    Updating declaration file

--- json-placeholder-replacer.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class JsonPlaceholderReplacer {
-    private variablesMap;
-    addVariableMap(variableMap: object): JsonPlaceholderReplacer;
-    replace(json: {}): {};
-    private replaceChildren;
-    replaceValue(node: string): string;
-    private checkInEveryMap(key);
-}
